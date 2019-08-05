c6599ebf6d816a0e45d232bacac62cfe524f2cf7
Nate Magera <nmagera07@gmail.com>
2019-08-05 15:29:57 -0400

    can fetch users from database

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // implement your API here
+const express = require('express')
+
+const Users = require('./data/db')
+
+const server = express()
+
+server.use(express.json())
+
+server.get('/', (req, res) => {
+    res.send('hello you can connect')
+})
+
+server.get('/users', (req, res) => {
+    Users.find()
+        .then(users => {
+            res.status(200).json(users)
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'error fetching list of users'})
+        })
+})
+
+
+
+const port = 8001
+server.listen(port, () => console.log('you are connected'))
