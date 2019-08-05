e46a832a71700fbf864b8563af54023cdc3e492f
Nate Magera <nmagera07@gmail.com>
2019-08-05 16:27:04 -0400

    finished routes for crud operations

--- index.js
+++ index.js
@@ -17,10 +17,75 @@ server.get('/users', (req, res) => {
             res.status(200).json(users)
         })
         .catch(error => {
-            res.status(500).json({ message: 'error fetching list of users'})
+            if (error) {
+                res.status(500).json({ error: 'The users information could not be retrieved.'})
+                // res.abort(500).json({ error: 'The users information could not be retrieved.'})
+            }
         })
 })
 
+server.get('/users/:id', (req, res) => {
+    const userId = req.params.id 
+    Users.findById(userId)
+        .then(user => {
+            if (user) {
+                res.status(200).json(user)
+            } else {
+                res.status(404).json({ message: 'The user with the specified ID does not exist'})
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'The user information could not be retrieved'})
+        })
+})
+
+server.delete('/users/:id', (req, res) => {
+    const userId = req.params.id 
+    Users.remove(userId)
+        .then(user => {
+            if (user) {
+                res.status(200).json( {user, message: 'user deleted successfully'} )
+            } else {
+                res.status(404).json({ message: 'The user with the specified ID does not exist.'})
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'user could not be deleted'})
+        })
+})
+
+server.post('/users', (req, res) => {
+    console.log('body data', req)
+    const userInfo = req.body
+    Users.insert(userInfo)
+        .then(user => {
+            
+            res.status(201).json(user)
+           
+                res.status(400).json({ errorMessage: 'Please provide name and bio for the user.'})
+            
+        })
+        .catch(error => {
+            res.status(500).json({ error: "There was an error while saving the user to the database" })
+        })
+})
+
+server.put('/users/:id', (req, res) => {
+    const { id } = req.params
+    const changes = req.body
+
+    Users.update(id, changes)
+        .then(updated => {
+            if (updated) {
+                res.status(200).json(updated)
+            } else {
+                res.status(404).json({ message: 'The user with the specified ID does not exist.'})
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ error: 'The user information could not be modified, sucka'})
+        })
+})
 
 
 const port = 8001
